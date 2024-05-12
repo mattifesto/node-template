db780a11579188ba2aa87bce07743e8560e72fb0
Matt Calkins <matt@mattifesto.com>
2024-05-12 22:47:33 +0000

    update test_project/index.mjs
    output a string if the add test passes

--- test_project/index.mjs
+++ test_project/index.mjs
@@ -5,4 +5,6 @@ let expectedResult = 3;
 
 if (result !== expectedResult) {
     throw new Error(`1 + 2 should equal 3, actual result was ${result}`);
+} else {
+    console.log("✅ add() test passed");
 }
